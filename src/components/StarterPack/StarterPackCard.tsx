e56b66061a0ed1b445b9d42a75ce8fd8399da497
surfdude29 <149612116+surfdude29@users.noreply.github.com>
2025-03-13 14:07:09 +0000

    Update StarterPackCard.tsx

--- src/components/StarterPack/StarterPackCard.tsx
+++ src/components/StarterPack/StarterPackCard.tsx
@@ -95,9 +95,8 @@ export function Card({
       ) : null}
       {!!joinedAllTimeCount && joinedAllTimeCount >= 50 && (
         <Text style={[a.font_bold, t.atoms.text_contrast_medium]}>
-          <Trans comment="Number of users (will always be at least 50) who have joined Bluesky using a specific starter pack">
-            <Plural value={joinedAllTimeCount} other="# users" />
-            {' '}have joined!
+          <Trans comment="Number of users (always at least 50) who have joined Bluesky using a specific starter pack">
+            <Plural value={joinedAllTimeCount} other="# users" /> have joined!
           </Trans>
         </Text>
       )}
